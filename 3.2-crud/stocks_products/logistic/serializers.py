d4fb1e6bf7c408d1a70732c691859dd5cc3aa3c0
agostev <gostev.av@gmail.com>
2023-03-11 17:07:12 +0300

    Completed

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,22 +1,38 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from logistic.models import Product, Stock, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
-
+    class Meta:
+        model = Product
+        fields = "__all__"
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ("product", "quantity", "price")
 
+    def validate_quantity(self, value):
+        if value < 0:
+            raise ValidationError("Количество меньше 0")
+        return value
+
+    def validate_price(self, value):
+        if value < 0:
+            raise ValidationError("Цена меньше 0")
+        return value
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ('id', 'address', 'positions')
 
     def create(self, validated_data):
+
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
 
@@ -26,6 +42,9 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+
+        for position in positions:
+            stock.positions.create(**position)
 
         return stock
 
@@ -39,5 +58,9 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=stock, product=position['product'],
+                                                  defaults={'price': position['price'],
+                                                            'quantity': position['quantity']})
 
         return stock
--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,16 +1,33 @@
+
+import django_filters.rest_framework
+
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
+class MyResultsSetPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+    pagination_class = MyResultsSetPagination
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    pagination_class = MyResultsSetPagination
+
     # при необходимости добавьте параметры фильтрации
+    # https://www.django-rest-framework.org/api-guide/filtering/
+
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['products']
